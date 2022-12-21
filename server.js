cbd05ec83dae629124c42d81a019a7749b1bcc1d
Jonas <jonas.claesson@unicus.com>
2022-12-21 13:23:59 +0100

    stuffs

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ const ello_guvnor = ["Hello"," world!"]
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('<html>')
+    res.write('<body>')
+    res.write('<p>')
+    res.write('test ')
+    res.write('</p>')
+    res.write('</body>')
+    res.write('</html>')
     res.end(ello_guvnor[0] + ello_guvnor[1]);
 }).listen(port);
