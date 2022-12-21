d38cd856b65313a6339892a743afe31d30011718
Jonas <jonas.claesson@unicus.com>
2022-12-21 14:27:27 +0100

    stuffs3: maybe funk

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var port = process.env.PORT || 1337;
 const ello_guvnor = ["Hello"," world!"]
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('test ')
-    res.end(ello_guvnor[0] + ello_guvnor[1]);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<p>')
+    res.write('test')
+    res.write('</p>')
+    res.end();
 }).listen(port);
